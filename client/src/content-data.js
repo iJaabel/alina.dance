48ff1564978f3e43c4a3b39d29b4885537f0bc1d
Jaabel <Jaabel@outlook.com>
2022-01-05 16:39:28 -0600

    redesigned architecture

---
+++ client/src/content-data.js
@@ -0,0 +1,20 @@
+const data = {
+  // Carousel Text Content ---
+  carouselTagline: [
+    //first picture
+    "I'm Alina",
+    //second picture
+    "I Love Dancing",
+    //third picture
+    "I Work Online",
+  ],
+
+  //about me
+  title: "ABOUT ME",
+  subTitle: "WHO AM I?",
+
+  //what I do
+  //my journey
+};
+
+export default data
