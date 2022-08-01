da3ceda526693be8d1c9cc01924ac0da494400f9
iJaabel <fp3@Dats-MacBook-Pro.local>
2022-08-01 09:24:32 -0500

    added files

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import {
+  Contact,
+  Intro,
+  Portfolio,
+  Work,
+  Testimonials,
+  Topbar,
+  Menu,
+} from "./components";
+import "./Global.scss";
+
+export default function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <div className="app">
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <div className="section">
+        <Intro />
+        <Portfolio />
+        {/* <Work />
+        <Testimonials /> */}
+        <Contact />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useRef } from "react";
+import "./Intro.scss";
+import { init } from "ityped";
+
+export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: [
+        "Zouk Instructor",
+        "Event Coordinator",
+        "Life Coach",
+        "Content Creator",
+        "Nutrition Advisor",
+        "eCommerce Specialist",
+        "Fitness Director",
+        "Family Coordinator",
+      ],
+    });
+  }, []);
+
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/A_flipped_intro.png" alt="Alina in a power pose" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Alina Pink</h1>
+          <h3>
+            The <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="a down arrow" />
+        </a>
+      </div>
+    </div>
+  );
+}
